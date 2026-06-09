7d3cf5e4696accb2cabc8c52e0dfd75d92ce9b80
Jovan Gabriel Ontiveros Perea <gabrielp@betterrx.com>
2026-06-08 22:08:31 -0600

    Add Vera react component

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './app.css';
+import '../css/app.css';
+import Vera from './Vera.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <div className="flex items-center justify-center min-h-screen bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] dark:text-[#EDEDEC]">
-            <h1 className="text-2xl font-medium">Hello from React</h1>
-        </div>
+        <Vera />
     </StrictMode>
 );
