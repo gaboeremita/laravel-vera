43b7a351e21ee1e1fff6c7ec8166066e9dfa6990
Jovan Gabriel Ontiveros Perea <gabrielp@betterrx.com>
2026-06-12 21:55:58 -0600

    Add conversation endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\Api\ChatController;
+use App\Http\Controllers\Api\ConversationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn (Request $request) => $request->user());
-    Route::post('/chat', [ChatController::class, 'send']);
+    Route::post('/conversations/{id}/messages', [ConversationController::class, 'sendMessage']);
+	Route::get('/conversations', [ConversationController::class, 'index']);
+	Route::get('/conversations/{id}/messages', [ConversationController::class, 'show']);
+	Route::post('/conversations', [ConversationController::class, 'store']);
 });
